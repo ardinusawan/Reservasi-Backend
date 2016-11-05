2ad257084b5ce2d60e5049eb104e48a3cc6d5dc0
Ardi Nusawan <ardi.nusawan13@gmail.com>
2016-11-06 02:47:27 +0700

    removing validation_by serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,3 +1,10 @@
 class BookingSerializer < ActiveModel::Serializer
   attributes :id, :user, :validation_by, :type, :description
+  # belongs_to :user, key: "validation_by", if: :validation_by?
+  #
+  # def validation_by?
+  #    true if object.validation_by!=0
+  #
+  # end
+
 end
