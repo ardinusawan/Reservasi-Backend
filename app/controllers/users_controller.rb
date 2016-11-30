4b8e9c011915171dac17c2247bc0e82583863b20
ardinusawan <ardi.nusawan13@gmail.com>
2016-11-30 20:03:42 +0700

    hiding password user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    render json: @users.as_json(:except => [:password])
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render json: @user.as_json(:except => [:password])
   end
 
   # Find Users
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
       @message.push("Message" => "User Not Found")
       render json: @message
     else
-      render json: @user
+      render json: @user.as_json(:except => [:password])
     end
   end
 
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user.as_json(:except => [:password]), status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render json: @user
+      render json: @user.as_json(:except => [:password])
     else
       render json: @user.errors, status: :unprocessable_entity
     end
