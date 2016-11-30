77092beb8179949b7bc9aefdeb992493d5c5df48
ardinusawan <ardi.nusawan13@gmail.com>
2016-12-01 01:49:32 +0700

    merapikan routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,23 @@
 Rails.application.routes.draw do
-  resources :schedules
-  # match 'schedules',to: 'schedules#options', via: :options
-  resources :bookings
-  # match 'bookings',to: 'bookings#options', via: :options
+  resources :schedules do
+    get 'now', to: 'schedules#now', on: :collection
+    get ':date', to: 'schedules#date', on: :collection
+  end
+  resources :bookings do
+    get 'unapproved', to: 'bookings#unapproved', on: :collection
+    get 'approved', to: 'bookings#approved', on: :collection
+  end
+  resources :users do
+    get 'nrp_nip/:nrp_nip', to: 'users#find_by_nrp_nip', on: :collection
+  end
   resources :types
-  # match 'types',to: 'types#options', via: :options
-  resources :users
-  # match 'users',to: 'users#options', via: :options
-  # match 'users', :controller => 'users', :action => 'options', via: :options
 
   match '*all',to: 'application#options', via: :options
 
-  get '/now', to: 'schedules#now'
-  get '/unapproved', to: 'bookings#unapproved'
-  get '/approved', to: 'bookings#approved'
-  get '/day/:date', to: 'schedules#day'
-  get '/find_user/:nrp_nip', to: 'users#find'
+
   # get '/day/:year/:month/:day' => 'schedule#day', constraints: {
   #     year:       /\d{4}/,
   #     month:      /\d{1,2}/,
   #     day:        /\d{1,2}/
   # }
-
-  # match 'options', to: 'bookings#options', via: :all
-
 end
