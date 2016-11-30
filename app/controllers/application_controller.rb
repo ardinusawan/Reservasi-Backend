916c3b60aa9538c4b2fe3a57dc8487ec71eeb80e
ardinusawan <ardi.nusawan13@gmail.com>
2016-11-30 15:08:21 +0700

    back to HTTP_ORIGIN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < ActionController::API
 
   private
   def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = request.env['http://ardinusawan.id']
+    headers['Access-Control-Allow-Origin'] = request.env['HTTP_ORIGIN']
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Max-Age'] = '1000'
     headers['Access-Control-Allow-Headers'] = '*,x-requested-with'
@@ -52,7 +52,7 @@ class ApplicationController < ActionController::API
 
   # HTTP_ORIGIN
   def access_allowed?
-    allowed_sites = [request.env['http://ardinusawan.id']] #you might query the DB or something, this is just an example
-    return allowed_sites.include?(request.env['http://ardinusawan.id'])
+    allowed_sites = [request.env['HTTP_ORIGIN']] #you might query the DB or something, this is just an example
+    return allowed_sites.include?(request.env['HTTP_ORIGIN'])
   end
 end
