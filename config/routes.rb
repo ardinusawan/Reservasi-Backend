4d3f5f01037b9a7472542589817e9d4e31badd55
ardinusawan <ardi.nusawan13@gmail.com>
2016-12-01 02:01:30 +0700

    remove to parameters if no needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :schedules do
-    get 'now', to: 'schedules#now', on: :collection
+    get 'now', on: :collection
     get ':date', to: 'schedules#date', on: :collection
   end
   resources :bookings do
-    get 'unapproved', to: 'bookings#unapproved', on: :collection
-    get 'approved', to: 'bookings#approved', on: :collection
+    get 'unapproved', on: :collection
+    get 'approved', on: :collection
   end
   resources :users do
     get 'nrp_nip/:nrp_nip', to: 'users#find_by_nrp_nip', on: :collection
