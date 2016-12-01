ddeaa1d5055e413ad1a287b964338597cd36dcd3
ardinusawan <ardi.nusawan13@gmail.com>
2016-12-01 18:15:04 +0700

    adding new scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,22 @@
 Rails.application.routes.draw do
-  resources :schedules do
-    get 'now', on: :collection
-    get ':date', to: 'schedules#date', on: :collection
-    post 'conflict', on: :collection
-  end
-  resources :bookings do
-    get 'unapproved', on: :collection
-    get 'approved', on: :collection
-  end
-  resources :users do
-    get 'nrp_nip/:nrp_nip', to: 'users#find_by_nrp_nip', on: :collection
-  end
-  resources :types
+  scope '/api' do
+    scope '/v1' do
+      resources :schedules do
+        get 'now', on: :collection
+        get ':date', to: 'schedules#date', on: :collection
+        post 'conflict', on: :collection
+      end
+      resources :bookings do
+        get 'unapproved', on: :collection
+        get 'approved', on: :collection
+      end
+      resources :users do
+        get 'nrp_nip/:nrp_nip', to: 'users#find_by_nrp_nip', on: :collection
+      end
+      resources :types
 
-  match '*all',to: 'application#options', via: :options
+      match '*all',to: 'application#options', via: :options
+    end
+  end
 
-
-  # get '/day/:year/:month/:day' => 'schedule#day', constraints: {
-  #     year:       /\d{4}/,
-  #     month:      /\d{1,2}/,
-  #     day:        /\d{1,2}/
-  # }
-end
+  end
