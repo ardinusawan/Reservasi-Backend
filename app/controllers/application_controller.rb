46dcda078e3d9a2339bade46c474fec7eaa24e6c
ardinusawan <ardi.nusawan13@gmail.com>
2016-12-04 02:42:40 +0700

    adding auth func

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+# Start of Auth
+  attr_reader :current_user
+
+  protected
+  def authenticate_request!
+    unless user_id_in_token?
+      render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+      return
+    end
+    @current_user = User.find(auth_token[:user_id])
+  rescue JWT::VerificationError, JWT::DecodeError
+    render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+  end
+
+  # End of Auth
+
+  # Start of CORS header
   def create
     if access_allowed?
       set_access_control_headers
@@ -31,4 +48,21 @@ class ApplicationController < ActionController::API
     allowed_sites = [request.env['HTTP_ORIGIN']] #you might query the DB or something, this is just an example
     return allowed_sites.include?(request.env['HTTP_ORIGIN'])
   end
+  # End of CORS header
+
+  # Start of Auth
+  def http_token
+    @http_token ||= if request.headers['Authorization'].present?
+                      request.headers['Authorization'].split(' ').last
+                    end
+  end
+
+  def auth_token
+    @auth_token ||= JsonWebToken.decode(http_token)
+  end
+
+  def user_id_in_token?
+    http_token && auth_token && auth_token[:user_id].to_i
+  end
+  # End of Auth
 end
