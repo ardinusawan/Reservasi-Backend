a9cf511402030e4dbdb7a612c3219520d058f53f
ardinusawan <ardi.nusawan13@gmail.com>
2016-12-04 20:17:39 +0700

    administrator if is_admin is true

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,7 @@
 class AuthenticationController < ApplicationController
   def authenticate_user
     user = User.find_for_database_authentication(email: params[:email])
-    if user.valid_password?(params[:password])
+    if user.valid_password?(params[:password]) and user.is_admin==true
       render json: payload(user)
     else
       render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
