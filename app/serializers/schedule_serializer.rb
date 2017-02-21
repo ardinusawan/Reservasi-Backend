502506b031ac29ab5acaf6f8ce8e16d00f536b5f
Ardi Nusawan <ardi.nusawan13@gmail.com>
2017-02-21 23:02:50 +0700

    adding nested value

--- app/serializers/schedule_serializer.rb
+++ app/serializers/schedule_serializer.rb
@@ -1,3 +1,9 @@
 class ScheduleSerializer < ActiveModel::Serializer
   attributes :id, :booking, :start, :end
+
+  belongs_to :booking, key: "booking", if: :booking?
+
+  def booking?
+    true if object.id!=0
+  end
 end
