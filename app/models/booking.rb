914908d37e9821d8aaeaf2531aaf46e2d221cea3
administrator <lp@if.its.ac.id>
2017-02-22 17:36:33 +0700

    error, nee more research

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :type
-  belongs_to :validation_by, :class_name => "User", :foreign_key => "validation_by"
+  #belongs_to :validation_by, :class_name => "User", :foreign_key => "validation_by"
 
 end
