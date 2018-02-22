fc400d8d3fca11d4c404d553d2ff0eaf6d622857
administrator <lp@if.its.ac.id>
2018-02-22 21:15:35 +0700

    update run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# cd /home/administrator/web/Reservasi-Backend
-gem update
-bundle install
-nohup rails s -b 0.0.0.0 -p 10003 -d -e production
+cd /home/administrator/web/Reservasi-Backend
+/root/.rbenv/shims/gem update
+/root/.rbenv/shims/bundle install
+nohup /root/.rbenv/shims/rails s -b 0.0.0.0 -p 10003 -d -e production
