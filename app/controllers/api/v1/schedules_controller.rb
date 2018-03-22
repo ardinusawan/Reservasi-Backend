18a63251ffd816cf6c1a758fae7318419beb336c
ardinusawan <ardi.nusawan13@gmail.com>
2018-03-23 01:19:00 +0700

    make check conflict of two date simple

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -9,59 +9,39 @@ module Api::V1
 
     def conflict?(date1, date2)
       # https://stackoverflow.com/a/325964/4154982
-      return ((date1.first <= date2.last) && (date1.last >= date2.first))
+      (date1.first <= date2.last) && (date1.last >= date2.first)
     end
 
     # POST /schedules/conflict
     def conflict
+      conflict  = Array.new
+      from = Time.parse(params[:start])
       schedules = Schedule.where("start >= ?", Time.zone.now.beginning_of_day)
-      schedules_conflict_list = Array.new
-      from = params[:start].to_datetime
-      to = params[:start].to_datetime + params[:duration].to_i.seconds
-      from = from.in_time_zone
-      to = to.in_time_zone
-      from = from - 7.hours
-      to = to - 7.hours
-      if params[:repeated]=="0"
-        tmp_from = from
-        tmp_to = to
-        $i = 0
-        schedules.each_with_index do |schedule, index|
-            if (conflict?([tmp_from, tmp_to],
-                          [schedule.start.to_datetime, schedule.end.to_datetime])) and $i < 1
-              @schedule_conflict = Schedule.find(schedule.id)
-              schedules_conflict_list.push($i => Array(@schedule_conflict))
-              $i +=1
-            end
-        end
-      else
-        tmp_from = from
-        tmp_to = to
-        $i = 0
-        while $i < params[:repeated_end_after].to_i            
-          schedules.each_with_index do |schedule, index|
-            if conflict?([tmp_from, tmp_to], [schedule.start, schedule.end])
-              @schedule_conflict = Schedule.find(schedule.id )
-              schedules_conflict_list.push(Array(@schedule_conflict))
-            end
+      for i in (1..params[:repeated_end_after].to_i)
+        to = from + params[:duration].to_i.seconds
+
+        schedules.each do |schedule|
+          if conflict?([from, to], [schedule.start.to_datetime, schedule.end.to_datetime])
+            conflict.push(Schedule.find(schedule.id))
           end
 
-          tmp_from += (params[:repeated_every].to_i.day if params[:repeated]=="1") || 
-                      (params[:repeated_every].to_i.week if params[:repeated]=="2") || 
-                      (params[:repeated_every].to_i.month if params[:repeated]=="3")
-          
-          tmp_to += (params[:repeated_every].to_i.day if params[:repeated]=="1") || 
-                    (params[:repeated_every].to_i.week if params[:repeated]=="2") || 
-                    (params[:repeated_every].to_i.month if params[:repeated]=="3")
-      
-          $i+=1
+        end
+
+        if params[:repeated]=="1"
+          from += params[:repeated_every].to_i.day
+        elsif params[:repeated]=="2"
+          from += params[:repeated_every].to_i.week
+        elsif params[:repeated]=="3"
+          from += params[:repeated_every].to_i.month
+        else
+          from = from
         end
       end
-      if schedules_conflict_list.none?
-        render :json => { :success => true}
-       else
-         render :json => { :success => false,
-                           :conflict => schedules_conflict_list }
+
+      if conflict.empty?
+        render json: {success: true, message: 'No Conflict!'}, status: :created
+      else
+        render json: {success: false, conflict: conflict}, status: :unprocessable_entity
       end
     end
 
@@ -117,6 +97,7 @@ module Api::V1
 
     # POST /schedules
     def create
+      inserted_data = Array.new
       conflict  = Array.new
       from = Time.parse(params[:start])
       schedules = Schedule.where("start >= ?", Time.zone.now.beginning_of_day)
@@ -138,6 +119,7 @@ module Api::V1
 
         if conflict.empty?
           @schedule = Schedule.new(data_insert)
+          inserted_data.push(@schedule)
           unless @schedule.save
             render json: {success: false, error: @schedule.error}, status: :unprocessable_entity
           end
@@ -155,7 +137,7 @@ module Api::V1
       end
 
       if conflict.empty?
-        render json: {success: true, data: @schedule}, status: :created
+        render json: {success: true, data: inserted_data}, status: :created
       else
         render json: {success: false, conflict: conflict}, status: :unprocessable_entity
       end
